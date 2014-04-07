7cc2c91f9626054ee3037e04ce564605043d0864
Matt McCormick <matt.mccormick@kitware.com>
2014-04-07 11:27:21 -0400

    ENH: AcquireIntersonBMode app skeleton.

---
+++ app/AcquireIntersonBMode.cxx
@@ -0,0 +1,12 @@
+#include "itkImageFileWriter.h"
+
+#include "AcquireIntersonBModeCLP.h"
+
+int main( int argc, char * argv[] )
+{
+  PARSE_ARGS;
+
+  const unsigned int Dimension = 3;
+
+  return EXIT_SUCCESS;
+}
